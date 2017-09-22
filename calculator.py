01ae0b59aff820c20f1be074a81b6838bb9e160d
Alexander <pass>
2017-09-22 14:30:58 +0200

    Add calculation of wall length and remove previous sketches
    Adds a function get_wall_length that using a buildings footprints
    calculates its circumference. Assuming each wall is a straight line
    between two adjacent points in a footprint list

--- calculator.py
+++ calculator.py
@@ -2,6 +2,7 @@ import csv
 import ast
 import pdb
 import sys
+import math
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -17,6 +18,9 @@ def load_data(dPath):
             except ValueError:
                 print('Skipping building {}'.format(row["BuildingId"]))
                 continue
+            if row["age_class"] == "-1":
+                print('Skipping building {} due unknown age class'.format(row['BuildingId']))
+                continue
             if row["Footprint"].startswith("[[["):
                 print('Skipping building {} due to invalid footprint, footprint is not a polygon'.format(row['BuildingId']))
                 continue
@@ -31,21 +35,22 @@ def load_data(dPath):
             buildings.loc[building_id] = [building_name, age_class, height, flats, type_building, footprint, heat_network_connected]
     return buildings
 
-def get_renovation_actions():
-    """ Returns a """
-
-def make_base_matrices(buildings):
-    for BuildingName, AgeClass, Height, Flats, TypeBuilding, Footprint, HeatNetworkConnected in buildings:
-        print(BuildingName)
-
-renovatedBuildings = dict()
-def renovate(building_id, buildings):
-    wallU, roofU, windowU, cellarU = None, None, None, None
-    
-
-    cost = area * specificRenovationCost
-    renovatedBuildings[building_id] = {'wall': wallU, 'roof': roofU, 'window': windowU, 'cellar': cellarU}
-
+def get_wall_length(building_footprint=None):
+    """ For a given, correct, building footprint calculates it's total wall length and returns float"""
+    length = 0
+    for i, point in enumerate(building_footprint):
+        x0 = building_footprint[i][0]
+        y0 = building_footprint[i][1]
+        print('Current footprint: x: {}, y: {}'.format(x0,y0))
+        try:
+            x1 = building_footprint[i+1][0]
+            y1 = building_footprint[i+1][1]
+        except IndexError:
+            x1 = building_footprint[0][0]
+            y1 = building_footprint[0][1]
+        partialLength = math.sqrt((x1-x0)**2+(y1-y0)**2)
+        length += partialLength
+    return length
 
 
 def make_solution_span(floor, ceiling, slices):
@@ -73,13 +78,14 @@ def plot_buildings(buildings):
 
 if __name__ == "__main__":
     buildings = load_data("BolzanoData/Building_0.csv")
-
+    
     window_ratio = {'SFH': 0.08,
                     'SMFH': 0.1,
                     'BMFH': 0.11,
                     'AB': 0.12
                     }
 
+    """
     d = 12 # 12 if all files
     for i in range(1, d):
         path = "BolzanoData/Building_{}.csv".format(i)
@@ -88,4 +94,4 @@ if __name__ == "__main__":
         buildings = pd.concat([buildings, temp])
     #pdb.set_trace()
     plot_buildings(buildings)
-        
+    """    
